2f496bf430d383526f4c342c1edf277e4c8c6196
Tencazzo <tencazzon@gmail.com>
2025-01-09 17:04:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algem_matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность системы уравнений (n):");
+            int n = int.Parse(Console.ReadLine());
+
+            double[,] A = new double[n, n];
+            double[] B = new double[n];
+
+            Console.WriteLine("Введите матрицу коэффициентов (A):");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите строку {i + 1} (через пробел):");
+                string[] input = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    A[i, j] = double.Parse(input[j]);
+                }
+            }
+
+            Console.WriteLine("Введите  свободные члены (B):");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите элемент {i + 1}:");
+                B[i] = double.Parse(Console.ReadLine());
+            }
+
+            double[] solution = Gauss(A, B);
+
+            Console.WriteLine("Решение системы уравнений:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"x{i + 1} = {solution[i]}");
+            }
+        }
+
+        static double[] Gauss(double[,] a, double[] b)
+        {
+            int n = b.Length;
+
+            // Прямой ход
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (a[j, i] == 0)
+                        continue;
+
+                    double ratio = a[j, i] / a[i, i];
+                    for (int k = i; k < n; k++)
+                    {
+                        a[j, k] -= ratio * a[i, k];
+                    }
+                    b[j] -= ratio * b[i];
+                }
+            }
+
+            // Обратный ход
+            double[] x = new double[n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                x[i] = b[i];
+                for (int j = i + 1; j < n; j++)
+                {
+                    x[i] -= a[i, j] * x[j];
+                }
+                x[i] /= a[i, i];
+            }
+
+            return x;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("algem_matrix")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("algem_matrix")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("53b559e0-04a2-42a3-9b39-89472dcf9ab0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
